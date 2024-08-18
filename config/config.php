121065b645b0b83cf91a63af98ceeb364bf50fc6
Tuomas Aalto <p40414@edu.sasky.fi>
2024-08-18 23:47:13 +0300

    linkit tapahtuma-sivulle ja etusivulle

--- config/config.php
+++ config/config.php
@@ -17,5 +17,5 @@ $config = array(
   define("TEMPLATE_DIR", PROJECT_ROOT . "src/view/");
   define("MODEL_DIR", PROJECT_ROOT . "src/model/");
   define("CONTROLLER_DIR", PROJECT_ROOT . "src/controller/");
-
+  define("BASEURL", $config['urls']['baseUrl']);
 ?>
