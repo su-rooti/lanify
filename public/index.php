53f6d714ea42ad9a946de247c9de085607b02327
Tuomas Aalto <p40414@edu.sasky.fi>
2024-08-18 23:18:28 +0300

    index.php korjaus

--- public/index.php
+++ public/index.php
@@ -9,16 +9,20 @@ require_once '../src/init.php';
   $request = str_replace($config['urls']['baseUrl'],'',$_SERVER['REQUEST_URI']);
   $request = strtok($request, '?');
 
-    // Luodaan uusi Plates-olio ja kytketään se sovelluksen sivupohjiin.
-    $templates = new League\Plates\Engine('../src/view');
+  // Selvitetään mitä sivua on kutsuttu ja suoritetaan sivua vastaava 
+  // Luodaan uusi Plates-olio ja kytketään se sovelluksen sivupohjiin.
+  $templates = new League\Plates\Engine('../src/view'); 
 
-    // Selvitetään mitä sivua on kutsuttu ja suoritetaan sivua vastaava
-    // käsittelijä.
-    if ($request === '/' || $request === '/tapahtumat') {
-      echo $templates->render('tapahtumat');
-    } else if ($request === '/tapahtuma') {
-      echo $templates->render('tapahtuma');
-    } else {
-      echo $templates->render('notfound');
-    }
+  // Selvitetään mitä sivua on kutsuttu ja suoritetaan sivua vastaava
+  // käsittelijä.
+  if ($request === '/' || $request === '/tapahtumat') {
+    echo '<h1>Kaikki tapahtumat</h1>';
+    echo $templates->render('tapahtumat');
+  } else if ($request === '/tapahtuma') {
+    echo '<h1>Yksittäisen tapahtuman tiedot</h1>';
+    echo $templates->render('tapahtuma');
+  } else {
+    echo '<h1>Pyydettyä sivua ei löytynyt :(</h1>';
+    echo $templates->render('notfound');
+  }
 ?> 
