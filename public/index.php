7c4ce171d82e8d1d20b9e6d681fcbaad28a16214
Tuomas Aalto <p40414@edu.sasky.fi>
2024-08-19 17:52:56 +0300

    henkilön lisääminen tietokantaan

--- public/index.php
+++ public/index.php
@@ -31,10 +31,16 @@ require_once '../src/init.php';
         echo $templates->render('tapahtumanotfound');
       }
       break;
-    case '/lisaa_tili':
-      echo $templates->render('lisaa_tili');
-      break;
-    default:
-      echo $templates->render('notfound');
+      case '/lisaa_tili':
+        if (isset($_POST['laheta'])) {
+          require_once MODEL_DIR . 'henkilo.php';
+          $salasana = password_hash($_POST['salasana1'], PASSWORD_DEFAULT);
+          $id = lisaaHenkilo($_POST['nimi'],$_POST['email'],$_POST['discord'],$salasana);
+          echo "Tili on luotu tunnisteella $id";
+          break;
+        } else {
+          echo $templates->render('lisaa_tili');
+          break;
+        }    ;
   }
 ?> 
