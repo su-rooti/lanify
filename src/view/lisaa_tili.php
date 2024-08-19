d3e16cdbdbc9e23187dbf2e4b2f235699e92c8e9
Tuomas Aalto <p40414@edu.sasky.fi>
2024-08-19 18:00:14 +0300

    lomakkeiden tarkastaminen

--- src/view/lisaa_tili.php
+++ src/view/lisaa_tili.php
@@ -5,23 +5,27 @@
 <form action="" method="POST">
   <div>
     <label for="nimi">Nimi:</label>
-    <input id="nimi" type="text" name="nimi">
+    <input id="nimi" type="text" name="nimi" value="<?= getValue($formdata,'nimi') ?>">
+    <div class="error"><?= getValue($error,'nimi'); ?></div>
   </div>
   <div>
     <label for="email">Sähköposti:</label>
-    <input id="email" type="email" name="email">
+    <input type="text" name="email" value="<?= getValue($formdata,'email') ?>">
+    <div class="error"><?= getValue($error,'email'); ?></div>
   </div>
   <div>
     <label for="discord">Discord-tunnus:</label>
-    <input id="discord" type="text" name="discord">
+    <input type="text" name="discord" value="<?= getValue($formdata,'discord')?>">
+    <div class="error"><?= getValue($error,'discord'); ?></div>
   </div>
   <div>
     <label for="salasana1">Salasana:</label>
-    <input id="salasana1" type="password" name="salasana1">
+    <input type="password" name="salasana1">
+    <div class="error"><?= getValue($error,'salasana'); ?></div>
   </div>
   <div>
     <label for="salasana2">Salasana uudelleen:</label>
-    <input id="salasana2" type="password" name="salasana2">
+    <input type="password" name="salasana2">
   </div>
   <div>
     <input type="submit" name="laheta" value="Luo tili">
