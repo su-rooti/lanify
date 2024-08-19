b1d8169bbfc88bbd18d8e0db73c451ac24373afc
Tuomas Aalto <p40414@edu.sasky.fi>
2024-08-19 18:14:12 +0300

    istunnon aloitus

--- public/index.php
+++ public/index.php
@@ -1,4 +1,6 @@
 <?php
+  // Aloitetaan istunnot.
+  session_start();
 
 // Suoritetaan projektin alustusskripti.
 require_once '../src/init.php';
@@ -50,7 +52,8 @@ require_once '../src/init.php';
           if (isset($_POST['laheta'])) {
             require_once CONTROLLER_DIR . 'kirjaudu.php';
             if (tarkistaKirjautuminen($_POST['email'],$_POST['salasana'])) {
-              echo "Kirjautuminen ok!";
+              $_SESSION['user'] = $_POST['email'];
+              header("Location: " . $config['urls']['baseUrl']);   
             } else {
               echo $templates->render('kirjaudu', [ 'error' => ['virhe' => 'Väärä käyttäjätunnus tai salasana!']]);
             }
