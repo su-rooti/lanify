845a36af63f72d71d525b749cd9299a1bbfdc2a4
Tuomas Aalto <p40414@edu.sasky.fi>
2024-08-19 18:15:31 +0300

    kirjautumistietojen tulostus

--- src/view/template.php
+++ src/view/template.php
@@ -6,8 +6,18 @@
     <meta charset="UTF-8">    
   </head>
   <body>
-    <header>
-    <h1><a href="<?=BASEURL?>">lanify</a></h1>
+  <header>
+      <h1><a href="<?=BASEURL?>">lanify</a></h1>
+      <div class="profile">
+        <?php
+          if (isset($_SESSION['user'])) {
+            echo "<div>$_SESSION[user]</div>";
+            echo "<div><a href='logout'>Kirjaudu ulos</a></div>";
+          } else {
+            echo "<div><a href='kirjaudu'>Kirjaudu</a></div>";
+          }
+        ?>
+      </div>
     </header>
     <section>
       <?=$this->section('content')?>
